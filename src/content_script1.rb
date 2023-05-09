2d266edf4b0fe7c8fa45e2f51b6e748a2a23ccf6
aaaa777(hiupc) <aaaa777@users.noreply.github.com>
2023-05-10 08:04:46 +0900

    edit content script

--- src/content_script1.rb
+++ src/content_script1.rb
@@ -1,6 +1,16 @@
 require "js"
 puts "Hello, this is content_script1!"
 
-# 見出しの文字を変える
+# change h1 text
 document = JS.global[:document]
 document[:querySelector].call(:call, document, "h1")[:innerText] = JS.try_convert("Rubyで動いたよ!")
+
+# same code in JavaScript
+# document.querySelector('h1').innerText = "Rubyで動いたよ!";
+
+
+# set background color red
+JS.global[:document][:body][:style][:backgroundColor] = JS.try_convert("red")
+
+# in JS
+# document.body.style.backgroundColor = "red";
--- src/content_script2.rb
+++ src/content_script2.rb
@@ -1,5 +1,2 @@
 require "js"
 puts "Hello, this is content_script2!"
-
-# 背景色を赤にする
-JS.global[:document][:body][:style][:backgroundColor] = JS.try_convert("red")
