14775b0d31ebd2879fd847a737448b9ad790c832
aaaa777(hiupc) <aaaa777@users.noreply.github.com>
2023-05-10 09:11:58 +0900

    add text

--- src/content_script.rb
+++ src/content_script.rb
@@ -3,7 +3,8 @@ puts "Hello, this is content_script!"
 
 # change h1 text
 document = JS.global[:document]
-document[:querySelector].call(:call, document, "h1")[:innerText] = JS.try_convert("Example Page with ruby.wasm!")
+text = JS.try_convert("Example Page with ruby.wasm!")
+document[:querySelector].call(:call, document, "h1")[:innerText] = text
 
 # same code in JavaScript
 # document.querySelector('h1').innerText = "Example Page with ruby.wasm!";
